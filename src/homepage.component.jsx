15218eb9346aba9c00ca833fb72229b0aa8fc9ae
Weblymit <weblymit@gmail.com>
2019-07-27 07:36:10 +0200

    created projet with create-react-app and add page homepage

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -8,7 +8,6 @@ const HomePage  = () => {
         <div className="menu-item">
           <div className="content">
             <h1 className="title">HATS</h1>
-            <h1>Hello</h1>
             <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
