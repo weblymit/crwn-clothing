95f400035186e83e5c20b7811d89f5f9e248f63e
Weblymit <weblymit@gmail.com>
2019-07-27 07:29:11 +0200

    test

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -8,6 +8,7 @@ const HomePage  = () => {
         <div className="menu-item">
           <div className="content">
             <h1 className="title">HATS</h1>
+            <h1>Hello</h1>
             <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
