f363f932ac80845f3b2d9a71056de7b5ccd6b00f
Weblymit <weblymit@gmail.com>
2019-08-05 16:10:14 +0200

    Add shop page, CollectionPreview and CollectionItem

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import Shopage from './pages/shop/shoppage.component'
 
 function App() {
 	return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}/>
-        {/* <Route path="/hat" component={HatsPage}/> */}
+        <Route path="/shop" component={Shopage}/>
       </Switch>
 			{/* <HomePage /> */}
 		</div>
