5b2bbc490ffa2e96de15d29666c143fdf8fd7a94
Weblymit <weblymit@gmail.com>
2019-08-05 16:35:27 +0200

    Adding Header and link to shopa page and contact

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import Shopage from './pages/shop/shoppage.component'
+import Header from './components/header-component/header.component'
 
 function App() {
 	return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route path="/shop" component={Shopage}/>
